a2de17a3e59f2094e30ee8a9434cb52d9b25849d
walteribanez555 <walteribanez555@gmail.com>
2024-10-08 14:37:24 -0400

    Template Project And Dependencies Added

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    colors: {
+      primary : '',
+      secondary : '',
+      muted : '',
+      foreground : ''
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
